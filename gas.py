9521ea81082b34b9a0a1bb6d5046ff8ed1b7960a
SeyedHosseinTafakh <colonel.hossein@gmail.com>
2019-03-16 17:22:05 +0330

    shandol

--- gas.py
+++ gas.py
@@ -119,8 +119,11 @@ class arazi(Resource):
         parser.add_argument("mablaghe_taeed_mali")
         parser.add_argument("tarikh_taeed_omoor_mali")
         parser.add_argument("stateDate")
-
+        parser.add_argument("peyvast",type=werkzeug.datastructures.FileStorage,location = 'files')
         args = parser.parse_args()
+        file = args['peyvast']
+        dirname = os.path.dirname(__file__)
+        file.save(os.path.join(dirname,'files',file.filename))
         sql = "INSERT INTO arazi (sharh , mablaghe_darkhasti_naftanir , mablaghe_hoghooghi,tarikh_hoghooghi ,mablaghe_taeed_mali ,tarikh_taeed_omoor_mali ,tarikh) VALUES (%s , %s , %s, %s,%s,%s,%s)"
         values = (args['sharh'] , args['mablaghe_darkhasti_naftanir'] , args['mablaghe_hoghooghi'] ,args['tarikh_hoghooghi'],args['mablaghe_taeed_mali'],args['tarikh_taeed_omoor_mali'],args['stateDate'],)
         db.mycursor.execute(sql , values)
@@ -149,13 +152,12 @@ class pipeLinesF(Resource):
         parser.add_argument('shomareGhalam')
         parser.add_argument('nerkhBank')
         parser.add_argument('hazineAnbar')
-        parser.add_argument('hazineSodoor')
+        parser.add_argument('hazineSodoorBime')
         parser.add_argument('hazineBime')
         args = parser.parse_args()
         
         # mysql = "INSERT INTO pipelinesf (tarikh ,zekhamat , metraj , tonaj , tarikhTahvil,typeKalaTahvil ,shomareHavaleAnbar , shomareTaghaza,shomareGhalam ,nerkhTashilBankMarkazi ,hazineAnbar , hazineSodoor, hazineBime , mablagheVaragh , avarezGomrok , hazineSakhteLoole , hazinePooshesh , maliatVaragh, maliatSakht )"
         mysql = "INSERT INTO pipelinesf (" \
-                "tarikh ," \
                 "zekhamat , " \
                 "metraj , " \
                 "tonaj , " \
@@ -166,7 +168,7 @@ class pipeLinesF(Resource):
                 "shomareGhalam ," \
                 "nerkhTashilBankMarkazi ," \
                 "hazineAnbar , " \
-                "hazineSodoorBime,) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+                "hazineSodoorBime) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
         values = (
                   args['zekhamat'],
                   args['metraj'],
@@ -178,7 +180,7 @@ class pipeLinesF(Resource):
                   args['shomareGhalam'],
                   args['nerkhBank'],
                   args['hazineAnbar'],
-                  args['hazineSodoor'],)
+                  args['hazineSodoorBime'],)
         db.mycursor.execute(mysql ,values)
         db.mydb.commit()
         return "saved"
@@ -188,19 +190,19 @@ class pipeLinesF(Resource):
         data = db.mycursor.fetchall()
         ret = {}
         for record in data:
-            mablaghe_varagh = int(record[4])*1033
-            avarez_gomrok = mablaghe_varagh * int(record[10]) * 4/100
+            mablaghe_varagh = int(record[3])*1033
+            avarez_gomrok = mablaghe_varagh * int(record[9]) * 4/100
             maliyat_bar_arzesh_afzoode_varagh = avarez_gomrok * 1/10
             hazine_sakht_loole = 0
-            if record[6] == "varagh" :
+            if record[5] == "ورق" :
                 hazine_sakht_loole = 0
             else :
-                hazine_sakht_loole = int(record[4]) * 125
+                hazine_sakht_loole = int(record[3]) * 125
             hazine_pooshesh = 0
-            if record[6] == "pooshesh":
-                hazine_pooshesh = int(record[4])*95
-            maliyat_bar_arzesh_afzoode_sakht_pooshesh = (hazine_sakht_loole + hazine_pooshesh) *  int(record[10]) * 10/100
-            motalebate_riyali = int(record[11]) + int(record[12]) + avarez_gomrok + maliyat_bar_arzesh_afzoode_varagh + maliyat_bar_arzesh_afzoode_sakht_pooshesh
+            if record[5] == "پوشش داده شده":
+                hazine_pooshesh = int(record[3])*95
+            maliyat_bar_arzesh_afzoode_sakht_pooshesh = (hazine_sakht_loole + hazine_pooshesh) *  int(record[9]) * 10/100
+            motalebate_riyali = int(record[10]) + int(record[11]) + avarez_gomrok + maliyat_bar_arzesh_afzoode_varagh + maliyat_bar_arzesh_afzoode_sakht_pooshesh
             motalebat_arzi = hazine_pooshesh + hazine_sakht_loole
 
             ret[record[0]] ={'dataBase' : record ,
@@ -229,12 +231,12 @@ class pardakht_naftanir(Resource):
         # parser = reqparse.RequestParser()
         # parser.add_argument("peyvast",type=werkzeug.datastructures.FileStorage)
         parser = reqparse.RequestParser()
-        parser.add_argument("peyvast",type=werkzeug.datastructures.FileStorage,location = 'files')
         parser.add_argument("date")
         parser.add_argument("sharh")
         parser.add_argument("dollar")
         parser.add_argument("riyal")
         parser.add_argument("tozihat")
+        parser.add_argument("peyvast",type=werkzeug.datastructures.FileStorage,location = 'files')
         args = parser.parse_args()
         file = args['peyvast']
         dirname = os.path.dirname(__file__)
@@ -368,6 +370,71 @@ class kala_30(Resource):
                             (args['estelam1'],args['estelam2'],args['estelam3'],))
         db.mydb.commit()
         return True
+
+class sadid_mahshahr(Resource):
+    def get(self):
+        db.mycursor.execute("SELECT * FROM sadid_mahshahr")
+        ret = db.mycursor.fetchall()
+        return ret
+    def post(self):
+        db.mycursor.execute("SELECT * FROM sadid_mahshahr")
+        DATA = db.mycursor.fetchall()
+        for d in DATA:
+            db.mycursor.execute("DELETE FROM sadid_mahshahr WHERE money = %s" ,(d[0],))
+            db.mydb.commit()
+        parse = reqparse.RequestParser()
+        parse.add_argument("money")
+        parse.add_argument("tik")
+        args = parse.parse_args()
+        db.mycursor.execute("INSERT INTO sadid_mahshahr(money , tik)  VALUES(%s , %s) " , (args['money'] , args['tik'],))
+        return True
+class jadval56(Resource):
+    def get(self):
+        p56 = pipeLinesF()
+        p56 =  p56.get()
+        p56 = json.dumps(p56)
+        p56 = json.loads(p56)
+        pipeline56 = {}
+        for id in  p56:
+             pipeline56[id] = {}
+             pipeline56[id]['taahod_be_pardakht'] = p56[id]['motalebat_riyali']
+             # jareme = jarime(1 ,1 ,1 ,1)
+             pipeline56[id]['jarime'] = 1
+             if int(id) == 1:
+                pndg = 0
+             else:
+                pndg = pipeline56[str(int(id) - 1)]['kole_motalebat']
+             pipeline56[id]['pardakht_nashode_dore_ghable'] = pndg
+             pipeline56[id]['kole_motalebat'] = int(pipeline56[id]['jarime']) + pndg + pipeline56[id]['taahod_be_pardakht']
+        return pipeline56
+class jadvalArazi(Resource):
+    def get(self):
+        arz = arazi()
+        arz = arz.get()
+        arz = json.dumps(arz)
+        arz = json.loads(arz)
+        jarazi = {}
+        for id in arz:
+            jarazi[id] ={}
+            jarazi[id]['pardakht_shode_tavasote_naftanir'] = arz[id][3]
+            jarazi[id]['tarikh_taaid_hoghooghi'] = arz[id][3]
+            jarazi[id]['taahod_be_pardakht_naftanir'] = 85747896194
+            jarazi[id]['jame_kole_mohasebat'] = int(jarazi[id]['taahod_be_pardakht_naftanir']) - int(arz[id][3])
+            jarazi[id]['jarime'] = "todo : havent add it"
+        return jarazi
+
+class looleSaziSadid(Resource):
+    def get(self):
+        # sadid = sadid_mahshahr()
+        # sadid = sadid.get()
+        # ret = {}
+        # ret['pardakht_shode_tavasote_naftanir'] = 0
+        # ret['taahod_be_pardakht'] = int(sadid[0][0]) / 3
+        # ret['pardakht_nashode_dore_ghabl'] = 0
+        # ret['jarime_dore_ghable'] = 0
+        # ret['kole_motalebat'] = int(ret['jarime_dore_ghable']) + int(ret['pardakht_nashode_dore_ghabl'])
+        return  "todo : havent done yet"
+
 api.add_resource(gostare,"/gostare")
 api.add_resource(comper,"/comperosor")
 api.add_resource(peymankaran,"/peymankaran")
@@ -376,5 +443,73 @@ api.add_resource(arazi , "/arazi")
 api.add_resource(pardakht_naftanir , "/pardakht_naftanir")
 api.add_resource(pardakht_shode_tavasote_naftanir , "/pardakht_shode_tavasote_naftanir_TM")
 api.add_resource(kala_30 , "/kala_30")
+api.add_resource(sadid_mahshahr , "/sadid_mahshahr")
+api.add_resource(jadval56 , "/jadval56")
+api.add_resource(jadvalArazi , "/jadvalArazi")
+api.add_resource(looleSaziSadid , "/jadval_loole_sazi_sadid")
+import locale , jdatetime
+from datetime import datetime , date
+def jarime(dore_ghable , nerkh_jarime , tarikh_khod , tarikh_ghabl):
+    # TODO:: have to complete this and i am sleepy and cant do it
+    return ekhtelaf_rooz()
+
+def ekhtelaf_rooz(datee1 , datee2):
+    sal = {}
+    sal[1] = 31
+    sal[2] = 31
+    sal[3] = 31
+    sal[4] = 31
+    sal[5] = 31
+    sal[6] = 31
+    sal[7] = 30
+    sal[8] = 30
+    sal[9] = 30
+    sal[10] = 30
+    sal[11] = 30
+    sal[12] = 29
+    date1 = datee1
+    date2 = datee2
+    date1 = date1.split('/')
+    date2 = date2.split('/')
+    if int(date1[0]) - int(date2[0]) == 0:
+        roozHa = int(date1[2]) - int(sal[int(date1[1])])
+        roozHa = abs(roozHa)
+        if int(date1[1]) - int(date2[1]) == 0:
+            rooHamoonMah = int(date2[2]) - int(sal[int(date2[1])])
+            roozHa = roozHa - abs(rooHamoonMah)
+            return roozHa
+        roozHa = roozHa + int(date2[2])
+        mah = int(date1[1]) - int(date2[1])
+        mah = abs(mah)
+        # return mah
+        mah_shomar = int(date1[2])
+        if mah != 0:
+            for i in range(mah):
+                roozHa = roozHa + int(sal[int(mah_shomar)])
+                mah_shomar = mah_shomar + 1
+                if mah_shomar == 12:
+                    mah_shomar = 1
+        return roozHa
+    if abs(int(date1[0]) - int(date2[0])) != 0:
+        roozHa = int(date1[2]) - int(sal[int(date1[1])])
+        roozHa = abs(roozHa)
+        roozHa = roozHa + int(date2[2])
+        dateee1 = datetime.strptime((datee1) , '%Y/%m/%d')
+        dateee2 = datetime.strptime((datee2) , '%Y/%m/%d')
+        from dateutil import relativedelta
+        diffs = relativedelta.relativedelta(dateee1, dateee2)
+        mah = abs(diffs.months)
+        print(mah)
+        mah = mah +  abs(int(diffs.months))
+        print(mah)
+        mah = mah + (abs(diffs.years) * 12 )
+        mah_shomar = int(date1[2])
+        if mah != 0:
+            for i in range(mah):
+                roozHa = roozHa + int(sal[int(mah_shomar)])
+                mah_shomar = mah_shomar + 1
+                if mah_shomar == 12:
+                    mah_shomar = 1
+        return roozHa
 
 app.run(debug=True)
